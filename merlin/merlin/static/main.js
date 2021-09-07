64d366bb714f96723ef3052233126534c242a9e7
John Capobianco <ptcapo@gmail.com>
2021-09-06 20:21:38 -0400

    workin

--- merlin/merlin/static/main.js
+++ merlin/merlin/static/main.js
@@ -36,3 +36,31 @@ $("#learn_vlan_button").click(function(e){
         }
     })
 })
+
+$("#learn_vrf_button").click(function(e){
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: "/OnDemand/learn_vrf_result/",
+        beforeSend: function(){
+            learnVRFSpinnerBox.classList.remove('not-visible')
+        },
+        success: function(response){
+            learnVRFSpinnerBox.classList.add('not-visible')
+        }
+    })
+})
+
+$("#show_version_button").click(function(e){
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: "/OnDemand/show_version_result/",
+        beforeSend: function(){
+            showVersionSpinnerBox.classList.remove('not-visible')
+        },
+        success: function(response){
+            showVersionSpinnerBox.classList.add('not-visible')
+        }
+    })
+})
