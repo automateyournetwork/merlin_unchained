6aaa83feb0320e6f1610ca1a4c75221b185819e9
John Capobianco <44117002+automateyournetwork@users.noreply.github.com>
2021-10-01 15:17:21 -0400

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,4 +6,4 @@ python3 manage.py collectstatic --noinput
 python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 redis-server --daemonize yes 
 redis-cli ping
-python3 waitress_server.py
+python3 manage.py runserver 0.0.0.0:8000
