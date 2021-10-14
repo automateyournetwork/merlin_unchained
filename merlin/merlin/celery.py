acc1ad1c6c3de4433d00ae1eef7de8d07b851e7a
John Capobianco <ptcapo@gmail.com>
2021-10-14 13:41:11 -0400

    flush

--- merlin/merlin/celery.py
+++ merlin/merlin/celery.py
@@ -1,17 +1,9 @@
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merlin.settings')
-
 app = Celery('merlin')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-
+# Celery settings are in settings.py using a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
@@ -19,14 +11,4 @@ app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
-
-app.conf.beat_schedule = {
-    #Scheduler Name
-    'run_pyATS_job_every_five_minutes': {
-        # Task Name (Name Specified in Decorator)
-        'task': 'run_pyATS_job',  
-        # Schedule      
-        'schedule': 300.0,
-    }
-}
+    print(f'Request: {self.request!r}')
