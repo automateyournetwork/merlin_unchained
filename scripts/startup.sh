eaa19354fffb0c9a601de1c58c0ee25e9c650f61
John Capobianco <ptcapo@gmail.com>
2021-11-10 14:59:43 -0500

    back in

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,9 +1,9 @@
-  cd merlin
-  sleep 120
-  python3 manage.py makemigrations merlin 
-  python3 manage.py migrate merlin
-  python3 manage.py migrate
-  python3 manage.py collectstatic --noinput
-  python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
-  python3 manage.py search_index --rebuild -f
-  python3 manage.py runserver 0.0.0.0:8000
+cd merlin
+sleep 120
+python3 manage.py makemigrations merlin
+python3 manage.py migrate merlin
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+python3 manage.py search_index --rebuild -f
+python3 manage.py runserver 0.0.0.0:8000
