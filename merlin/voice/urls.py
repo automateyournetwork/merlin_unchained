84f8a3ceb6511c9079595f785b716a269f9f1e51
John Capobianco <ptcapo@gmail.com>
2021-11-22 17:20:35 -0500

    working at interface level

--- merlin/voice/urls.py
+++ merlin/voice/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('Voice/', views.voice_page),
     path('Voice/Learn/', views.learn_voice_page),
     path('Voice/Show/', views.show_voice_page),
-    path('Voice/Config/', views.config_voice_page),
+    path('Voice/<str:pyats_alias>/<str:interface>/Config/', views.config_voice_page, name="config_voice"),
 ]
--- merlin/voice_config.py
+++ merlin/voice_config.py
@@ -10,12 +10,17 @@
 from pyats import aetest
 from pyats import topology
 from pyats.log.utils import banner
-from merlin.models import DynamicJobInput
+from merlin.models import DynamicJobInput, InterfaceEnable
 from jinja2 import Environment, FileSystemLoader
 
 template_dir = 'merlin/templates/Jinja2'
 env = Environment(loader=FileSystemLoader(template_dir))
 voice_command_template = env.get_template('voice_command.j2')
+voice_config_db = DynamicJobInput.objects.all().values('input_field')
+voice_config = voice_config_db[0]['input_field']
+interface_to_config_key = InterfaceEnable.objects.all().values('interface')
+interface_to_config = interface_to_config_key[0]['interface']
+
 # ----------------
 # AE Test Setup
 # ----------------
@@ -39,13 +44,13 @@ class Collect_Information(aetest.Testcase):
         # Loop over devices
         # ---------------------------------------
         for device in testbed.devices.values():
-            voice_config_db = DynamicJobInput.objects.all().values('input_field')
-            voice_config = voice_config_db[0]['input_field']
-            templated_voice_command = voice_command_template.render(voice_command=voice_config)
+            templated_voice_command = voice_command_template.render(voice_command=voice_config,interface=interface_to_config)
             with steps.start("Voice Command", continue_=True) as step:
                 try:
                     device.configure(templated_voice_command)
-                    delete_records = DynamicJobInput.objects.all()
-                    delete_records.delete()
+                    delete_job_records = DynamicJobInput.objects.all()
+                    delete_job_records.delete()
+                    delete_interface_records = InterfaceEnable.objects.all()
+                    delete_interface_records.delete()                    
                 except Exception as e:
                     step.failed('Could not shut the interface correctly\n{e}'.format(e=e))
